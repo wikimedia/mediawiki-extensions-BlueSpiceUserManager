764fa4e5c19f42d4887a659cf9c9b7a9c20e9c38
Philipp Miguel Pirzer <fugnirzer@gmail.com>
2017-11-22 14:18:22 +0100

    BlueSpiceUserManager: Change licence and version
    Changed GPL-version to 3.0.
    
    Raised extension version number to 3.0.0-alpha.
    
    Added COPYING-file.
    
    Corrected licence-disclaimer.
    
    Removed @version-comments.
    
    Compuser.json is correct.
    
    Change-Id: Ic45d57415ade64103b29454b2047c6c201cda108

--- resources/bluespice.userManager.js
+++ resources/bluespice.userManager.js
@@ -2,11 +2,10 @@
  * UserManager extension
  *
  * @author     Stephan Muggli <muggli@hallowelt.com>
- * @version    2.22.0
  * @package    Bluespice_Extensions
  * @subpackage UserManager
  * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v3
  * @filesource
  */
 
