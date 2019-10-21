ed4d88c3303e81fdf7fae94ce82e142080ebf032
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-10-21 01:13:37 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 28.0.0
    Change-Id: Ibaf54a15c265afbd776b284ee187a075a49b93c4

--- tests/phpunit/BSApiChangeableGroupStoreTest.php
+++ tests/phpunit/BSApiChangeableGroupStoreTest.php
@@ -31,7 +31,7 @@ class BSApiChangeableGroupStoreTest extends BSApiExtJSStoreTestBase {
 	protected function createStoreFixtureData() {
 	}
 
-	protected function setUp() {
+	protected function setUp() : void {
 		parent::setUp();
 		$this->mergeMwGlobalArrayValue(
 			'wgGroupPermissions',
