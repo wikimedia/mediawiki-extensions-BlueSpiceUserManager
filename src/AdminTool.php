60428fe631dff750a24ed20b2bd6003cfc89fc83
hslater <hamish.slater@hallowelt.com>
2025-01-28 15:23:12 +0100

    Update SpecialPage namespace
    ERM38444
    
    [REL1_43, master]

--- src/AdminTool.php
+++ src/AdminTool.php
@@ -4,6 +4,7 @@ namespace BlueSpice\UserManager;
 
 use BlueSpice\IAdminTool;
 use MediaWiki\Message\Message;
+use MediaWiki\SpecialPage\SpecialPage;
 
 class AdminTool implements IAdminTool {
 
@@ -12,7 +13,7 @@ class AdminTool implements IAdminTool {
 	 * @return string String of the URL
 	 */
 	public function getURL() {
-		$tool = \SpecialPage::getTitleFor( 'UserManager' );
+		$tool = SpecialPage::getTitleFor( 'UserManager' );
 		return $tool->getLocalURL();
 	}
 
