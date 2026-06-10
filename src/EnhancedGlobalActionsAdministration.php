701e0b33c6f01102ab66b140065423f1fa7ffe1d
d.savuljesku <d.savuljesku@gmail.com>
2026-06-10 11:52:33 +0200

    In a farm, open UserManager on root instance
    [5.3][galaxy]
    
    ERM48005
    
    Change-Id: Ia680e5785f992181a5df78c002aa53580aaf7af9

--- src/EnhancedGlobalActionsAdministration.php
+++ src/EnhancedGlobalActionsAdministration.php
@@ -5,8 +5,18 @@ namespace BlueSpice\UserManager;
 use HtmlArmor;
 use MediaWiki\Html\Html;
 use MediaWiki\Message\Message;
+use MediaWiki\Title\TitleFactory;
 
 class EnhancedGlobalActionsAdministration extends GlobalActionsAdministration {
+
+	/**
+	 * @param TitleFactory $titleFactory
+	 */
+	public function __construct(
+		private readonly TitleFactory $titleFactory
+	) {
+		parent::__construct();
+	}
 
 	/**
 	 * @inheritDoc
@@ -16,5 +26,17 @@ class EnhancedGlobalActionsAdministration extends GlobalActionsAdministration {
 			'class' => 'badge rounded-pill text-bg-secondary'
 		], Message::newFromKey( 'bs-usermanager-global-label' )->text() );
 		return new HtmlArmor( $html );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		if ( defined( 'FARMER_IS_ROOT_WIKI_CALL' ) && FARMER_IS_ROOT_WIKI_CALL ) {
+			$title = $this->titleFactory->makeTitle( NS_SPECIAL, 'UserManager' );
+			return $title->getLocalURL();
+		}
+		$title = $this->titleFactory->newFromText( 'w:Special:UserManager' );
+		return $title->getFullURL();
 	}
 }
